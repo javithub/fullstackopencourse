98c99af2c5d1363dd98aee90afcd9ce10186d15f
Javier Devesa <javier.devesa@delonia.com>
2020-01-11 08:30:41 +0100

    fix path error accesing .env file

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,7 +1,11 @@
-require('dotenv').config()
+require('dotenv').config({path: './blog.env'});
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
 
 module.exports = {
   MONGODB_URI,
