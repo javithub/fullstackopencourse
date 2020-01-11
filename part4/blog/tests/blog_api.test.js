f550ad7557ae0d280041ae9d6442547b9c2011af
Javier Devesa <javier.devesa@delonia.com>
2020-01-11 09:18:17 +0100

    setting the database for tests

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      title: 'Blog de futbol (test)',
+      author: 'Zidane',
+      url: 'football.blogspot.com',
+      likes: 10,
+    },
+    {
+        title: 'Blog de baloncesto (test)',
+        author: 'Chicho Sibilio',
+        url: 'basketball.blogspot.com',
+        likes: 7,
+    },
+  ]
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
 
 describe('Blog API test', () => {
     test('blogs are returned as json', async () => {
@@ -12,16 +37,19 @@ describe('Blog API test', () => {
         .expect('Content-Type', /application\/json/)
     })
 
-    test('there are two blogs', async () => {
+    test('all blogs are returned', async () => {
         const response = await api.get('/api/blogs')
     
-        expect(response.body.length).toBe(2)
+        expect(response.body.length).toBe(initialBlogs.length)
     })
     
-    test('the first blog is about football', async () => {
+    test('a specific blog is within the returned blogs', async () => {
         const response = await api.get('/api/blogs')
     
-        expect(response.body[0].title).toBe('Blog de futbol (test database)')
+        const titles = response.body.map(r => r.title)
+        expect(titles).toContain(
+            'Blog de futbol (test)'
+          )
     })
 
     afterAll(() => {
